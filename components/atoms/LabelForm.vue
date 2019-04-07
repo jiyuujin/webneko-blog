7426eaae2c23d4ccd0a60664cde41f2621884e07
jiyuujin LAB <jiyuujinunite@gmail.com>
2019-04-07 12:29:31 +0900

    タグ機能を追加 #6

---
+++ components/atoms/LabelForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <label class="tag-text">
+    {{ tagText }}
+  </label>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+
+@Component({})
+export default class LabelForm extends Vue {
+  @Prop() tagText!: string;
+}
+</script>
+
+<style scoped>
+.tag-text {
+  border: medium solid #d3d3d3;
+  border-radius: 24px;
+  overflow: hidden;
+  font-weight: bold;
+  cursor: pointer;
+  transition: all 0.2s ease-out;
+  letter-spacing: 0;
+  font-size: 0.75rem;
+  margin: 12px 4px;
+  padding: 0.4rem 0.8rem;
+  height: 24px;
+  line-height: 12px;
+  text-align: center;
+  display: inline-block;
+  vertical-align: middle;
+  align-items: center;
+}
+
+.tag-text:hover {
+  border: medium solid #ff4500;
+}
+</style>
---
+++ components/post/Tags.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <title-label
+      :is-center="isCenter"
+      :is-bold="isBold"
+      width="40%"
+      horizontal-margin="30%"
+      color="#42b883"
+    >
+      Tags
+    </title-label>
+    <div class="tags">
+    <span
+      v-for="tag in tags"
+      :key="tag"
+    >
+      <label-form
+        :tag-text="tag"
+      />
+    </span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+const TitleLabel = () => import('~/components/atoms/TitleLabel.vue')
+const LabelForm = () => import('~/components/atoms/LabelForm.vue')
+
+@Component({
+  components: {
+    TitleLabel,
+    LabelForm
+  }
+})
+export default class Tags extends Vue {
+  get tags() {
+    return this.$store.state.product.tags
+  }
+
+  isCenter: boolean = true;
+  isBold: boolean = true;
+}
+</script>
+
+<style scoped>
+.tags {
+  width: 100%;
+  text-align: center;
+}
+</style>
