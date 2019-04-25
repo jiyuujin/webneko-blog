48ef22699504bc3ff8e39be3a47e9763e6638714
jiyuujin LAB <jiyuujinunite@gmail.com>
2019-04-25 09:56:53 +0900

    プライバシーポリシーを問い合わせのページに移動

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -5,11 +5,19 @@
         お問い合わせ
       </h2>
       <p>
-        お仕事のご依頼や当ブログに対するご提案などご気軽に。
+        お仕事のご依頼や当ブログに対するご提案などご気軽に
       </p>
       <new
         category="manual"
       />
+      <div class="privacy-policy">
+        <div class="privacy-policy-title">
+          プライバシーポリシー
+        </div>
+        <p>
+          <privacy-policy />
+        </p>
+      </div>
     </div>
   </main-template>
 </template>
@@ -18,11 +26,13 @@
 import { Component, Vue } from 'nuxt-property-decorator'
 const MainTemplate = () => import('~/components/templates/MainTemplate.vue')
 const New = () => import('~/components/contact/New.vue')
+const PrivacyPolicy = () => import('~/components/atoms/PrivacyPolicy.vue')
 
 @Component({
   components: {
+    MainTemplate,
     New,
-    MainTemplate
+    PrivacyPolicy
   },
   head() {
     return {
@@ -37,5 +47,28 @@ export default class Index extends Vue {}
 <style scoped>
 .contact {
   text-align: center;
+}
+
+.privacy-policy {
+  width: 84%;
+  margin: 12px 8%;
+  background: #f1f1f1;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.22);
+}
+
+.privacy-policy .privacy-policy-title {
+  font-size: 1.2em;
+  background: #42b883;
+  padding: 4px;
+  text-align: center;
+  color: #FFF;
+  font-weight: bold;
+  letter-spacing: 0.05em;
+}
+
+.privacy-policy p {
+  padding: 15px 20px;
+  margin: 0;
+  text-align: left;
 }
 </style>
