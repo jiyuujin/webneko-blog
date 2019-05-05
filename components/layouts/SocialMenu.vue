37fa708f080a77acbe86a8f44444a93b0e58db19
jiyuujin LAB <jiyuujinunite@gmail.com>
2019-05-05 21:58:44 +0900

    ソーシャルボタンを追加

---
+++ components/layouts/SocialMenu.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="social-menu">
+    <a
+      :href="`http://twitter.com/share?text=${title}&url=https://webneko.info/posts/${slugText}`"
+      onClick="window.open(encodeURI(decodeURI(this.href)), 'tweetwindow', 'width=650, height=470, personalbar=0, toolbar=0, scrollbars=1, sizable=1'); return false;"
+    >
+      <img
+        src="../../static/twitter.png"
+        alt="twitter"
+      >
+    </a>
+    <a
+      :href="`http://b.hatena.ne.jp/entry/webneko.info/posts/${slugText}`"
+      onClick="window.open(encodeURI(decodeURI(this.href)), 'tweetwindow', 'width=650, height=470, personalbar=0, toolbar=0, scrollbars=1, sizable=1'); return false;"
+    >
+      <img
+        src="../../static/hatena.png"
+        alt="hatena"
+      >
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+
+@Component({})
+export default class SocialMenu extends Vue {
+  @Prop() slugText: string;
+  @Prop() title: string;
+}
+</script>
+
+<style scoped>
+.social-menu {
+  position: sticky;
+  left: 2%;
+  top: 60px;
+  z-index: 100;
+  width: 4%;
+  height: 0;
+}
+
+.social-menu img {
+  width: 24px;
+  margin: 8px;
+}
+
+@media (max-width: 500px) {
+  .social-menu {
+    display: none;
+  }
+}
+</style>
