108643456f5f3187987449ebef070582205dd87e
jiyuujin LAB <jiyuujinunite@gmail.com>
2019-08-11 12:55:50 +0900

    lint周りの修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,17 @@
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    node: true,
-    es6: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
-    sourceType: 'module'
-  },
-  extends: [
-    'eslint:recommended'
-  ],
-  // required to lint *.vue files
-  plugins: [
-    'vue',
-    '@typescript-eslint'
-  ],
-  // add your custom rules here
+  env: { node: true },
+  extends: ['eslint:recommended'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    '@typescript-eslint/adjacent-overload-signatures': 'error',
+    'no-unused-vars': 1,
+    strictFunctionTypes: false,
+    typeof: false,
     'no-undef': 'error',
-    'typeof': false,
-    'strictFunctionTypes': false,
-    'no-unused-vars': 'off'
+    '@typescript-eslint/adjacent-overload-signatures': 'error'
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
   }
 }
