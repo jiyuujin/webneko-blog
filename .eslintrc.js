a9a40a8f025e705a51346fe2563b0ee9aec2d8c9
yuma kitamura <yuma.kitamura@smaregi.jp>
2019-09-13 09:44:40 +0900

    lint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,17 @@
 module.exports = {
   root: true,
   env: { node: true },
-  extends: ['eslint:recommended'],
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue/issues/746#issuecomment-451112435
+    'plugin:vue/base'
+    // 'eslint:recommended'
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-unused-vars': 1,
-    strictFunctionTypes: false,
-    typeof: false,
+    strictFunctionTypes: 0,
+    typeof: 0,
     'no-undef': 'error'
   },
   parserOptions: {
