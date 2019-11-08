61e35f82fdf83144d29a2b4a385b37621be5493e
jiyuujin LAB <jiyuujinunite@gmail.com>
2019-11-08 19:32:02 +0900

    アドベントカレンダーのページを追加

---
+++ pages/advent-calendar/_year.vue
@@ -0,0 +1,28 @@
+<template>
+  <advent-calendar
+    :items="adventCalendars"
+    :year="$route.params.year"
+  />
+</template>
+
+<script>
+import Vue from 'vue'
+import AdventCalendar from '@/components/AdventCalendar.vue'
+
+export default Vue.extend({
+    components: {
+        AdventCalendar
+    },
+    computed: {
+        adventCalendars() {
+            return this.$store.state.product.archives
+        }
+    },
+    async asyncData({ store, route }) {
+        await store.dispatch('product/fetchPost', {
+            'slug': '',
+            'month': `${route.params.year}-12`
+        })
+    }
+})
+</script>
