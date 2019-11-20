1b282d318cf6f0fd757f7584e4d5ac9d3eaaaca7
jiyuujin <yuma.kitamura@smaregi.jp>
2019-11-20 16:48:08 +0900

    アクセシビリティ向上を目指す

--- pages/index.vue
+++ pages/index.vue
@@ -15,13 +15,13 @@
               <div class="title">
                 {{ post.fields.title }}
               </div>
-              <div class="date">
-                {{ getDate(post.fields.publishDate) }}
-              </div>
-              <div class="description">
-                {{ post.fields.description }}
-              </div>
             </nuxt-link>
+            <div class="date">
+              {{ getDate(post.fields.publishDate) }}
+            </div>
+            <div class="description">
+              {{ post.fields.description }}
+            </div>
           </template>
         </div>
       </div>
@@ -101,7 +101,7 @@ export default Vue.extend({
   line-height: 1.2;
   height: 4.8em;
   text-align: left;
-  color: #ccc;
+  color: #aaa;
   display: flex;
   align-items: center;
 }
