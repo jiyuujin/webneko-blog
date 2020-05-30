f1ea807debf7cb3008db5184a7ba50c092a43553
jiyuujin LAB <jiyuujinunite@gmail.com>
2020-05-30 12:06:50 +0900

    Add sanitize

--- components/LoadedMarkdown.vue
+++ components/LoadedMarkdown.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="body">
-    <div class="post-detail" v-html="$md.render(body)" />
+    <div class="post-detail" v-html="$md.render($sanitize(body))" />
   </div>
 </template>
 
@@ -15,7 +15,7 @@ export default Vue.extend({
     }
   },
   async mounted() {
-    ;(this as any).$microlinkjs('.link-preview')
+    this.$microlinkjs('.link-preview')
   }
 })
 </script>
