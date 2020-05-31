a2c85863fd98955d4fdbad923d43e7f0c1fcbf59
jiyuujin LAB <jiyuujinunite@gmail.com>
2020-05-31 11:40:35 +0900

    update page

--- components/NewContact.vue
+++ components/NewContact.vue
@@ -1,30 +1,50 @@
 <template>
   <div class="contact">
+    <div class="contact-title">
+      お問い合わせ
+    </div>
+
     <j-form title="タイトル">
-      <j-input :text="form.title" width="100%" @handleInput="applyTitle" />
+      <div style="text-align: left;">
+        <j-input :text="form.title" width="100%" @handleInput="applyTitle" />
+      </div>
     </j-form>
     <j-form v-if="category === 'manual'" title="カテゴリー">
-      <j-select
-        :options="contactCategories"
-        :values="form.contactCategory"
-        @handleSelect="applyContactCategory"
-      />
+      <div style="text-align: left;">
+        <j-select
+          :options="contactCategories"
+          :values="form.contactCategory"
+          @handleSelect="applyContactCategory"
+        />
+      </div>
     </j-form>
     <j-form title="メールアドレス">
-      <j-input :text="form.email" width="100%" @handleInput="applyEmail" />
+      <div style="text-align: left;">
+        <j-input :text="form.email" width="100%" @handleInput="applyEmail" />
+      </div>
     </j-form>
     <j-form title="詳細">
-      <j-input
-        :text="form.description"
-        width="100%"
-        @handleInput="applyDescription"
-      />
+      <div style="text-align: left;">
+        <j-input
+          :text="form.description"
+          width="100%"
+          @handleInput="applyDescription"
+        />
+      </div>
     </j-form>
     <j-form>
-      <j-button text="送信します" fill="text" @handleClick="submit" />
-      <span v-if="responseText">
-        {{ responseText }}
-      </span>
+      <div style="text-align: left;">
+        <j-button text="送信します" fill="text" @handleClick="submit" />
+        <span>
+          <a
+            href="https://nekohack-privacy-policy.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            プライバシーポリシーはこちらで
+          </a>
+        </span>
+      </div>
     </j-form>
   </div>
 </template>
