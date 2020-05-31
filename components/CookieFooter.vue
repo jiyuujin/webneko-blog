2e4951444e6d1b4950d5264baed6b02e1c779253
jiyuujin LAB <jiyuujinunite@gmail.com>
2020-05-31 10:27:24 +0900

    set url

--- components/CookieFooter.vue
+++ components/CookieFooter.vue
@@ -1,6 +1,7 @@
 <template>
   <j-cookie-footer
     :title="message"
+    :url="url"
     @accept="$store.dispatch('product/accept', true)"
   />
 </template>
@@ -12,7 +13,8 @@ export default Vue.extend({
   data() {
     return {
       message:
-        'あなたがこのサイト上でベストな体験ができるようCookieを使用しています'
+        'あなたがこのサイト上でベストな体験ができるようCookieを使用しています',
+      url: 'https://nekohack-privacy-policy.netlify.app/'
     }
   }
 })
