af8fd3e6c27f59037816305cc4365e99e5803589
jiyuujin <jiyuujinunite@gmail.com>
2021-02-17 19:07:27 +0900

    refactor: update design for mode change

--- components/HeaderText.vue
+++ components/HeaderText.vue
@@ -3,12 +3,17 @@
     <div class="author">
       <h1>Web猫ブログ</h1>
     </div>
+    <mode-change key="top-mode-change" />
   </div>
 </template>
 
 <script lang="ts">
+const ModeChange = () => import('~/components/ModeChange.vue')
+
 export default {
-  //
+  components: {
+    ModeChange
+  }
 }
 </script>
 
