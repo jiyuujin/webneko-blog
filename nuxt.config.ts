3eebd0c68ac05e4e0205e45bdb38535d16a88fc5
jiyuujin <jiyuujinunite@gmail.com>
2021-09-25 11:09:02 +0900

    feat: ogImage by Vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -321,5 +321,6 @@ export default {
     CTF_BLOG_POST_TYPE_ID: process.env.CTF_BLOG_POST_TYPE_ID,
     CTF_SPACE_ID: process.env.CTF_SPACE_ID,
     CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN,
+    OG_BASIC_ENDPOINT: process.env.OG_BASIC_ENDPOINT,
   },
 }
