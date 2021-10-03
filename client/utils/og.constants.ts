ee457cc98b437d6a2a6a16ecff4c8021d9e574f4
jiyuujin <jiyuujinunite@gmail.com>
2021-10-03 09:21:33 +0900

    fix: og property

--- client/utils/og.constants.ts
+++ client/utils/og.constants.ts
@@ -1,41 +1,41 @@
 export const generalOg = (title?: string, description?: string, url?: string, image?: string) => [
   {
     hid: 'description',
-    name: 'description',
+    property: 'description',
     content: description
       ? description
       : '2018年10月より運営の当ブログを始め、Vue.jsやNuxtを使ったフロントエンドを中心に設計・開発しています。',
   },
   {
     hid: 'og:site_name',
-    name: 'og:site_name',
+    property: 'og:site_name',
     content: title ? title : 'Web猫ブログ',
   },
   {
     hid: 'og:type',
-    name: 'og:type',
+    property: 'og:type',
     content: 'website',
   },
   {
     hid: 'og:description',
-    name: 'og:description',
+    property: 'og:description',
     content: description
       ? description
       : '2018年10月より運営の当ブログを始め、Vue.jsやNuxtを使ったフロントエンドを中心に設計・開発しています。',
   },
   {
     hid: 'og:title',
-    name: 'og:title',
+    property: 'og:title',
     content: title ? title : 'Web猫ブログ',
   },
   {
     hid: 'og:url',
-    name: 'og:url',
+    property: 'og:url',
     content: url ? url : 'https://webneko.dev/',
   },
   {
     hid: 'og:image',
-    name: 'og:image',
+    property: 'og:image',
     // content: `${Endpoints.BASE_URL}bakeneko-ogp.jpg`,
     content: image,
   },
@@ -44,39 +44,39 @@ export const generalOg = (title?: string, description?: string, url?: string, im
 export const twitterOg = (title?: string, description?: string, image?: string) => [
   {
     hid: 'twitter:description',
-    name: 'twitter:description',
+    property: 'twitter:description',
     content: description
       ? description
       : '2018年10月より運営の当ブログを始め、Vue.jsやNuxtを使ったフロントエンドを中心に設計・開発しています。',
   },
   {
     hid: 'twitter:title',
-    name: 'twitter:title',
+    property: 'twitter:title',
     content: title ? title : 'Web猫ブログ',
   },
   {
     hid: 'twitter:card',
-    name: 'twitter:card',
+    property: 'twitter:card',
     content: 'summary_large_image', // ex: summary, summary_large_image
   },
   {
     hid: 'twitter:site',
-    name: 'twitter:site',
+    property: 'twitter:site',
     content: '@jiyuujinlab',
   },
   {
     hid: 'twitter:creator',
-    name: 'twitter:creator',
+    property: 'twitter:creator',
     content: '@jiyuujinlab',
   },
   {
     hid: 'twitter:site',
-    name: 'twitter:site',
+    property: 'twitter:site',
     content: '@jiyuujinlab',
   },
   {
     hid: 'twitter:image',
-    name: 'twitter:image',
+    property: 'twitter:image',
     // content: `${Endpoints.BASE_URL}bakeneko-ogp.jpg`,
     content: image,
   },
