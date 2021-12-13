bebdc4b08ac51d7bfd56991581be37318dc74f1b
jiyuujin <jiyuujinunite@gmail.com>
2021-12-13 19:59:11 +0900

    use anchor links instead

--- client/utils/constants.ts
+++ client/utils/constants.ts
@@ -2,7 +2,7 @@ import Endpoints from '~/utils/endpoints.constants'
 
 export const CONTACT_URL =
   'https://docs.google.com/forms/d/e/1FAIpQLSfFzwsCVnFbu-lV0Jz2fEYHR97hpBSK2g6kEwc-G1fo976ngA/viewform'
-export const SOCIAL_LIST = ['twitter', 'hatena', 'note', 'share']
+export const SOCIAL_LIST = ['twitter', 'hatena', 'note']
 export const TWITTER_URL = (title: string, slug: string) =>
   `http://twitter.com/share?text=${title}&url=${Endpoints.POST_BASE_URL}${slug}`
 export const HATENA_URL = (slug: string) => `http://b.hatena.ne.jp/entry/webneko.dev/posts/${slug}`
