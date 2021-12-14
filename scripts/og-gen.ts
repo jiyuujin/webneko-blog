84be2f396b51e1c6ca68c5b4f13cc8c24323b410
jiyuujin <jiyuujinunite@gmail.com>
2021-12-14 10:46:50 +0900

    add reaction to og image

--- scripts/og-gen.ts
+++ scripts/og-gen.ts
@@ -9,7 +9,7 @@ const OG_SOURCE_DIR_PATH = path.join(__dirname, '..', 'data')
 const OG_SOURCE_HTML_FILE_PATH = path.join(OG_SOURCE_DIR_PATH, 'og.html')
 const OG_DIR_PATH = path.join(__dirname, '..', 'client', 'static', 'og')
 
-async function captureOgImage(browser: any, title: string, imagePath: string) {
+async function captureOgImage(browser: any, title: string, reaction: string, imagePath: string) {
   const page = await browser.newPage()
   await page.setViewport({
     width: 1200,
@@ -17,6 +17,9 @@ async function captureOgImage(browser: any, title: string, imagePath: string) {
   })
   await page.goto('file://' + OG_SOURCE_HTML_FILE_PATH)
   await page.exposeFunction('getTitle', () => title)
+  if (reaction) {
+    await page.exposeFunction('getReaction', () => reaction)
+  }
   await page.reload()
   await page.screenshot({
     path: imagePath,
@@ -30,7 +33,7 @@ async function captureOgImage(browser: any, title: string, imagePath: string) {
 
   const jsonObject = JSON.parse(await fs.readFileSync('./webneko-blog-export.json', 'utf8'))
 
-  let ogList = [{ title: 'Web猫ブログ', slug: 'top' }]
+  let ogList = [{ title: 'Web猫ブログ', slug: 'top', reaction: '' }]
 
   Object(jsonObject?.entries).forEach((obj: unknown | any) => {
     const field = obj.fields
@@ -39,15 +42,17 @@ async function captureOgImage(browser: any, title: string, imagePath: string) {
 
       const slugText = field.slug[lang]
 
-      ogList.push({ title: titleText, slug: slugText })
+      const reactionText = field.reaction[lang]
+
+      ogList.push({ title: titleText, slug: slugText, reaction: reactionText })
     }
   })
 
   const browser = await puppeteer.launch()
 
-  for (const { title, slug } of ogList) {
+  for (const { title, slug, reaction } of ogList) {
     const OG_IMAGE_FILE_PATH = `${OG_DIR_PATH}/` + slug + '.jpg'
-    await captureOgImage(browser, title, OG_IMAGE_FILE_PATH)
+    await captureOgImage(browser, title, reaction, OG_IMAGE_FILE_PATH)
   }
 
   await browser.close()
