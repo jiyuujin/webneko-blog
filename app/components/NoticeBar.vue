cc8a9394536645a76accee353b391f96e9866f11
jiyuujin <jiyuujinunite@gmail.com>
2022-03-21 22:37:19 +0900

    add notice bar

---
+++ app/components/NoticeBar.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="notice-bar">
+    <a href="https://twitter.com/jiyuujinlab" target="_blank" rel="noopener noreferrer">
+      jiyuujin
+    </a>
+    {{ `is united with the people of Ukraine and the international community.` }}
+  </div>
+</template>
