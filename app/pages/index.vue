4eb67cf564c97237bea370710c7532d19c00bde7
jiyuujin <jiyuujinunite@gmail.com>
2022-03-27 22:17:38 +0900

    og setup

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -11,10 +11,19 @@
 <script setup lang="ts">
 import { useAsyncData } from '#app'
 import { fetchPosts } from '~/api/blog'
+import { generalOg, twitterOg } from '~/utils/og.constants'
 
 import PostCard from '~/components/PostCard.vue'
 
 const { data: posts } = await useAsyncData('posts', () => {
   return fetchPosts()
+})
+
+useMeta({
+  title: 'Web猫ブログ',
+  meta: [
+    ...generalOg(),
+    ...twitterOg()
+  ]
 })
 </script>
